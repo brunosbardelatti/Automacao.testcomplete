434f071a4584ec9f248f29bf45e51159e5750dcd
Bruno Sbardelatti <pro69@UNI4.local>
2019-04-23 17:39:06 -0300

    inserido Login

--- Vendas Caixa/TestProject1/Script/Login.py
+++ Vendas Caixa/TestProject1/Script/Login.py
@@ -1,8 +1,10 @@
 ﻿import time 
 def login():
-  BuiltIn.ShowMessage("certifiquese que o caixa esta em contingencia.")
+  BuiltIn.ShowMessage("certifiquese que o caixa está configurada, para o correto cupom fiscal a ser executado.")
   TestedApps.HiperPdv.Run()
   time.sleep(1)
+  #-------------Inserir o Usuario correto para Logar----------------------------------------------------------
   Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtUsuario.SetText("nfce")
-  Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtUsuario.Keys("[Tab]")
-  Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtSenha.Keys("123[Enter]")
+  #-------------Inserir o senha  correto para Logar-----------------------------------------------------------
+  Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtSenha.Keys("123")
+  Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtSenha.Keys("[Enter]")
