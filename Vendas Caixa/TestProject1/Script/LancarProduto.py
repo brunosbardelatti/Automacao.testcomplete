56f0750a9ca8682cf5444ae808e0648a87a250cf
Bruno Sbardelatti <bruno.sbardelatti@gmail.com>
2020-03-12 15:23:21 -0300

    Ajustes de execução

--- Vendas Caixa/TestProject1/Script/LancarProduto.py
+++ Vendas Caixa/TestProject1/Script/LancarProduto.py
@@ -1,10 +1,10 @@
 ﻿def LancarProdutoSequencia():
-  cod =3001
+  cod =3020
   Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtEntradaDados.SetText(cod)
   Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtEntradaDados.Keys("[Enter]")
   if (Aliases.HiperPdv.HwndSource_BaseWindow.BaseWindow.cboPessoa.Exists):
     Aliases.HiperPdv.HwndSource_BaseWindow.BaseWindow.cboPessoa.Keys("[Esc]")
-  while cod < 3011:
+  while cod < 3035:
    Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtEntradaDados.SetText(cod)
    Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.txtEntradaDados.Keys("[Enter]")
    Aliases.HiperPdv.HwndSource_MainWindow.MainWindow.TabPrincipal.numQuantidade.Keys("[Enter]")
